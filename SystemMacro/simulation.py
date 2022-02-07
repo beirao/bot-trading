434c06c50c6e3a73218f2adb7a52c2b68224d76f
Beirao <thomasmarques911@gmail.com>
2022-02-08 00:51:56 +0100

    red pill mais ca va

--- SystemMacro/simulation.py
+++ SystemMacro/simulation.py
@@ -20,7 +20,7 @@ from initModel import Net,initModel
 def round_tensor(t, decimal_places=3):
   return round(t.item(), decimal_places)
 
-def simulation(netl,x,walletUSD,threshold,p):
+def simulationThreshold(netl,x,walletUSD,threshold,p):
     y_pred = netl(x)
 
     fee = 0.001
@@ -33,18 +33,14 @@ def simulation(netl,x,walletUSD,threshold,p):
     py=[]
     py = np.array(py)
     fig = plt.figure(figsize=(13,10))
-    ax1 = fig.add_subplot(111)
-    ax2 = ax1.twiny()
 
     for i in range(len(x)) :
         if y_pred[i] > 0.5 + threshold:
             if not buy :
                 soldeCOIN = (walletUSD/p[i])-(walletUSD/p[i]*fee)
                 sumFee += walletUSD/p[i]*fee
-                #plt.axvline(i,color='green')
                 buy = True
-
-            py = np.append(py,(soldeCOIN*p[i]))
+                plt.axvline(i,color='green')
             nbTrade = nbTrade + 1
 
         elif y_pred[i] < 0.5 - threshold:
@@ -52,15 +48,67 @@ def simulation(netl,x,walletUSD,threshold,p):
                 walletUSD = (p[i]*soldeCOIN) - (p[i]*soldeCOIN)*fee
                 sumFee += (p[i]*soldeCOIN)*fee
                 buy = False
-                #plt.axvline(i,color='red')
-            py = np.append(py,walletUSD)
+                plt.axvline(i,color='red')
 
+        if buy :
+             py = np.append(py,(soldeCOIN*p[i]))
+        else :
+            py = np.append(py,walletUSD)
 
 
     plt.title('Evolution du wallet')
     plt.yscale("log")
-    ax1.plot(py,'r',label="wallet evolution from 1000 USD")
-    ax2.plot(p, label="prix asset")
+    plt.plot(py,'r',label="wallet evolution from 1000 USD")
+    plt.plot(p, label="prix asset")
+    fig.legend()
+    plt.show()
+
+    print("\nwallet HOLD : ",(soldeCOIN_HOLD*p[-1]))
+    print("wallet final : ", (walletUSD))
+    print("nb trade : ",nbTrade)
+    print("frais payé : ",sumFee)
+    print("\n")
+
+def simulationConsecutive(netl,x,walletUSD, threshold, p):
+    y_pred = netl(x)
+
+    fee = 0.001
+    buy = False
+    nbTrade = 0
+    soldeCOIN = 0
+    soldeCOIN_HOLD = (walletUSD/p[0])-(walletUSD/p[0]*fee)
+    sumFee = 0
+
+    py=[]
+    py = np.array(py)
+    fig = plt.figure(figsize=(16,8))
+
+    for i in range(len(x)) :
+        if y_pred[i] > 0.5 + threshold and y_pred[i-1] > 0.5  + threshold and y_pred[i-2] > 0.5 + threshold :
+            if not buy :
+                soldeCOIN = (walletUSD/p[i])-(walletUSD/p[i]*fee)
+                sumFee += walletUSD/p[i]*fee
+                buy = True
+                plt.axvline(i,color='green')
+            nbTrade = nbTrade + 1
+
+        elif y_pred[i] < 0.5 - threshold and y_pred[i-1] < 0.5 - threshold and y_pred[i-2] < 0.5 - threshold :
+            if buy :
+                walletUSD = (p[i]*soldeCOIN) - (p[i]*soldeCOIN)*fee
+                sumFee += (p[i]*soldeCOIN)*fee
+                buy = False
+                plt.axvline(i,color='red')
+
+        if buy :
+            py = np.append(py,(soldeCOIN*p[i]))
+        else :
+            py = np.append(py,walletUSD)
+
+
+    plt.title('Evolution du wallet')
+    plt.yscale("log")
+    plt.plot(py,'r',label="wallet evolution from 1000 USD")
+    plt.plot(p, label="prix asset")
     fig.legend()
     plt.show()
 
@@ -86,7 +134,8 @@ model = model.to("cpu")
 
 
 #%% simu ------------------------------
-simulation(model,x,1000,0.4,p)
+#♠simulationThreshold(model,x,1000,0.4,p)
+simulationConsecutive(model,x,1000, 0.3,p)
 
 
 
